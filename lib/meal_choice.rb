a16ed3dd3a959e23d90c219e048b1867b57b321d
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-07 03:27:34 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,10 +1,5 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
-def meal_choice(veg1, veg2, protein*)
-    return "What a nutritious meal!" && "A plate of #{protein} with #{veg1} and #{veg2}. "
-    if protein='meat'
-elsif protein()
-return "What a nutritous meal!" && "A plate of #{protein} with #{veg1} and #{veg2}."
-end
-puts meal_choice("broccoli", "macaroni", "tofu")
+def meal_choice(veg1, veg2, protein='meat')
+  puts 
