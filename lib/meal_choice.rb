1b572067090e5c58e7ea43cf3a8063ab1e58f29a
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-07 04:09:56 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,17 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+
+def meal_choice(veg1, veg2, protein='meat')
+  yum = "What a nutritious meal!"
+  puts yum
+  
+  order = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts order
+end 
+  
+  return yum order(meal_choice("broccoli", "macaroni")) 
+  return yum order(meal_choice("broccoli", "macaroni", "tofu"))
+
+
+
+  
